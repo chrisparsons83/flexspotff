f5723314ca25cc33a4361ab90b39ef87ef6c122d
Chris Parsons <chris@chris-parsons.com>
2022-07-25 21:58:21 -0700

    Type narrowing param provider.

--- app/routes/auth/$provider.callback.tsx
+++ app/routes/auth/$provider.callback.tsx
@@ -1,7 +1,9 @@
 import type { LoaderArgs } from "@remix-run/node";
+import invariant from "tiny-invariant";
 import { authenticator } from "~/auth.server";
 
 export let loader = ({ request, params }: LoaderArgs) => {
+  invariant(params.provider, "This is not a valid provider");
   return authenticator.authenticate(params.provider, request, {
     successRedirect: "/dashboard",
     failureRedirect: "/login",
--- app/routes/auth/$provider.tsx
+++ app/routes/auth/$provider.tsx
@@ -1,9 +1,11 @@
 import type { ActionArgs } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
+import invariant from "tiny-invariant";
 import { authenticator } from "~/auth.server";
 
 export let loader = () => redirect("/login");
 
 export let action = ({ request, params }: ActionArgs) => {
+  invariant(params.provider, "This is not a valid provider");
   return authenticator.authenticate(params.provider, request);
 };
