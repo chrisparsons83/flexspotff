c2c222c3e5c538678291388896ccfd7af2e5b1fd
Chris Parsons <chris@chris-parsons.com>
2022-07-27 23:02:59 -0700

    Login protect dashboard.

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,3 +1,19 @@
+import type { LoaderArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { authenticator } from "~/auth.server";
+
+type LoaderData = {
+  user: unknown;
+};
+
+export const loader = async ({ request }: LoaderArgs) => {
+  let user = await authenticator.isAuthenticated(request, {
+    failureRedirect: "/login",
+  });
+
+  return json<LoaderData>({ user });
+};
+
 export default function Dashboard() {
   return <div>Dashboard!</div>;
 }
