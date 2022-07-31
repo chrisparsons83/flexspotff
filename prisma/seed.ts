8982499ee2ddedcbf1d9550edab8787560da5bd3
Chris Parsons <chris@chris-parsons.com>
2022-07-31 00:08:40 -0700

    Making the TS compiler not hate me.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,26 +1,22 @@
 import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 
 async function seed() {
-  const email = "rachel@remix.run";
+  const discordId = "1111";
+  const discordName = "JoshGordon";
+  const discordAvatar = "";
 
   // cleanup the existing database
-  await prisma.user.delete({ where: { email } }).catch(() => {
+  await prisma.user.delete({ where: { discordId } }).catch(() => {
     // no worries if it doesn't exist yet
   });
 
-  const hashedPassword = await bcrypt.hash("racheliscool", 10);
-
   const user = await prisma.user.create({
     data: {
-      email,
-      password: {
-        create: {
-          hash: hashedPassword,
-        },
-      },
+      discordId,
+      discordName,
+      discordAvatar,
     },
   });
 
