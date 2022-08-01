f542b5e85adbf702c6c842b536fd291020f8efb5
Chris Parsons <chris@chris-parsons.com>
2022-07-31 22:08:33 -0700

    Fix login page.

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,14 +1,26 @@
+import type { LoaderArgs } from "@remix-run/node";
 import { Form } from "@remix-run/react";
 import { SocialsProvider } from "remix-auth-socials";
+import { authenticator } from "~/auth.server";
 
 interface SocialButtonProps {
   provider: SocialsProvider;
   label: string;
 }
 
+export const loader = async ({ request }: LoaderArgs) => {
+  await authenticator.isAuthenticated(request, {
+    successRedirect: "/dashboard",
+  });
+
+  return {};
+};
+
 const SocialButton: React.FC<SocialButtonProps> = ({ provider, label }) => (
   <Form action={`/auth/${provider}`} method="post">
-    <button>{label}</button>
+    <button className="focus-visible:ring-offset-2zd inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500">
+      {label}
+    </button>
   </Form>
 );
 
