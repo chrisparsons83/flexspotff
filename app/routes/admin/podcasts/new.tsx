43b1959c6ec10834a466c4cc1fcc0c9bde4628a2
Chris Parsons <chris@chris-parsons.com>
2022-08-05 22:51:35 -0700

    Finish initial form structure for adding podcast.

--- app/routes/admin/podcasts/new.tsx
+++ app/routes/admin/podcasts/new.tsx
@@ -4,7 +4,7 @@ import {
   unstable_parseMultipartFormData,
 } from "@remix-run/node";
 import type { ActionArgs, UploadHandler } from "@remix-run/node";
-import { Form, useActionData } from "@remix-run/react";
+import { Form, useActionData, useTransition } from "@remix-run/react";
 import Button from "~/components/Button";
 import { redirect } from "~/utils/data";
 import { s3UploadHandler } from "~/services/s3client.server";
@@ -15,16 +15,27 @@ type ActionData = {
     title: string | undefined;
     season: string | undefined;
     episode: string | undefined;
+    description: string | undefined;
+    showNotes: string | undefined;
+    publishDate: string | undefined;
     podcastFile: string | undefined;
+    durationMinutes: string | undefined;
+    durationSeconds: string | undefined;
   };
   fields?: {
     title: string;
     season: string;
     episode: string;
-    podcastFile: string | undefined;
+    description: string;
+    showNotes: string;
+    publishDate: string;
+    podcastFile: string;
+    durationMinutes: string;
+    durationSeconds: string;
   };
 };
 
+// TODO: Bring this into a settings page
 const SEASONS = [2, 1];
 
 export const action = async ({
@@ -39,13 +50,21 @@ export const action = async ({
     uploadHandler
   );
 
-  console.log(formData);
+  console.log(formData.get("podcastFile"));
 
   return redirect(`/admin/podcasts/new`);
 };
 
 export default function PodcastEpisodeCreate() {
   const actionData = useActionData<ActionData>();
+  const transition = useTransition();
+
+  const buttonText =
+    transition.state === "submitting"
+      ? "Adding..."
+      : transition.state === "loading"
+      ? "Added!"
+      : "Add";
 
   return (
     <>
@@ -61,6 +80,7 @@ export default function PodcastEpisodeCreate() {
               Title:
               <input
                 type="text"
+                required
                 defaultValue={actionData?.fields?.title}
                 name="title"
                 id="title"
@@ -84,11 +104,12 @@ export default function PodcastEpisodeCreate() {
             ) : null}
           </div>
           <div>
-            <label>
+            <label htmlFor="season">
               Season:
               <select
                 defaultValue={actionData?.fields?.season}
                 name="season"
+                required
                 aria-invalid={
                   Boolean(actionData?.fieldErrors?.season) || undefined
                 }
@@ -99,14 +120,7 @@ export default function PodcastEpisodeCreate() {
               >
                 {SEASONS.map((season) => {
                   return (
-                    <option
-                      value={season}
-                      key={season}
-                      selected={
-                        !!actionData?.fields?.season &&
-                        season === Number.parseInt(actionData?.fields?.season)
-                      }
-                    >
+                    <option value={season} key={season}>
                       {season}
                     </option>
                   );
@@ -128,6 +142,7 @@ export default function PodcastEpisodeCreate() {
               Episode:
               <input
                 type="number"
+                required
                 min="1"
                 defaultValue={actionData?.fields?.episode}
                 name="episode"
@@ -152,10 +167,101 @@ export default function PodcastEpisodeCreate() {
             ) : null}
           </div>
           <div>
-            <label htmlFor="episode">
+            <label htmlFor="description">
+              Description:
+              <textarea
+                name="description"
+                required
+                id="description"
+                defaultValue={actionData?.fields?.description}
+                aria-invalid={
+                  Boolean(actionData?.fieldErrors?.description) || undefined
+                }
+                aria-errormessage={
+                  actionData?.fieldErrors?.description
+                    ? "description-error"
+                    : undefined
+                }
+                className="mt-1 block w-full dark:border-0 dark:bg-slate-800"
+                rows={3}
+              ></textarea>
+            </label>
+            {actionData?.fieldErrors?.description ? (
+              <p
+                className="form-validation-error"
+                role="alert"
+                id="description-error"
+              >
+                {actionData.fieldErrors.description}
+              </p>
+            ) : null}
+          </div>
+          <div>
+            <label htmlFor="showNotes">
+              Show Notes:
+              <textarea
+                name="showNotes"
+                required
+                id="showNotes"
+                defaultValue={actionData?.fields?.showNotes}
+                aria-invalid={
+                  Boolean(actionData?.fieldErrors?.showNotes) || undefined
+                }
+                aria-errormessage={
+                  actionData?.fieldErrors?.showNotes
+                    ? "showNotes-error"
+                    : undefined
+                }
+                className="mt-1 block w-full dark:border-0 dark:bg-slate-800"
+                rows={3}
+              ></textarea>
+            </label>
+            {actionData?.fieldErrors?.showNotes ? (
+              <p
+                className="form-validation-error"
+                role="alert"
+                id="showNotes-error"
+              >
+                {actionData.fieldErrors.showNotes}
+              </p>
+            ) : null}
+          </div>
+          <div>
+            <label htmlFor="publishDate">
+              Publish Date:
+              <input
+                type="date"
+                defaultValue={actionData?.fields?.publishDate}
+                name="publishDate"
+                required
+                id="publishDate"
+                aria-invalid={
+                  Boolean(actionData?.fieldErrors?.publishDate) || undefined
+                }
+                aria-errormessage={
+                  actionData?.fieldErrors?.publishDate
+                    ? "publishDate-error"
+                    : undefined
+                }
+                className="mt-1 block w-full dark:border-0 dark:bg-slate-800"
+              />
+            </label>
+            {actionData?.fieldErrors?.publishDate ? (
+              <p
+                className="form-validation-error"
+                role="alert"
+                id="publishDate-error"
+              >
+                {actionData.fieldErrors.publishDate}
+              </p>
+            ) : null}
+          </div>
+          <div>
+            <label htmlFor="podcastFile">
               Podcast File:
               <input
                 type="file"
+                required
                 defaultValue={actionData?.fields?.podcastFile}
                 name="podcastFile"
                 id="podcastFile"
@@ -181,12 +287,82 @@ export default function PodcastEpisodeCreate() {
             ) : null}
           </div>
           <div>
+            <label htmlFor="durationMinutes">
+              Duration:{" "}
+              <span className="text-sm italic text-slate-400">
+                (mm:ss format)
+              </span>
+              <div className="flex">
+                <div className="shrink basis-1/2">
+                  <input
+                    type="number"
+                    min={0}
+                    defaultValue={actionData?.fields?.durationMinutes}
+                    name="durationMinutes"
+                    required
+                    id="durationMinutes"
+                    aria-invalid={
+                      Boolean(actionData?.fieldErrors?.durationMinutes) ||
+                      undefined
+                    }
+                    aria-errormessage={
+                      actionData?.fieldErrors?.durationMinutes
+                        ? "durationMinutes-error"
+                        : undefined
+                    }
+                    className="mt-1 block w-full dark:border-0 dark:bg-slate-800"
+                  />
+                </div>
+                <div className="grow px-2">:</div>
+                <div className="shrink basis-1/2">
+                  <input
+                    type="number"
+                    min={0}
+                    max={59}
+                    defaultValue={actionData?.fields?.durationSeconds}
+                    name="durationSeconds"
+                    required
+                    id="durationSeconds"
+                    aria-invalid={
+                      Boolean(actionData?.fieldErrors?.durationSeconds) ||
+                      undefined
+                    }
+                    aria-errormessage={
+                      actionData?.fieldErrors?.durationSeconds
+                        ? "durationSeconds-error"
+                        : undefined
+                    }
+                    className="mt-1 block w-full dark:border-0 dark:bg-slate-800"
+                  />
+                </div>
+              </div>
+            </label>
+            {actionData?.fieldErrors?.durationMinutes ? (
+              <p
+                className="form-validation-error"
+                role="alert"
+                id="durationMinutes-error"
+              >
+                {actionData.fieldErrors.durationMinutes}
+              </p>
+            ) : null}
+            {actionData?.fieldErrors?.durationSeconds ? (
+              <p
+                className="form-validation-error"
+                role="alert"
+                id="durationSeconds-error"
+              >
+                {actionData.fieldErrors.durationSeconds}
+              </p>
+            ) : null}
+          </div>
+          <div>
             {actionData?.formError ? (
               <p className="form-validation-error" role="alert">
                 {actionData.formError}
               </p>
             ) : null}
-            <Button type="submit">Add</Button>
+            <Button type="submit">{buttonText}</Button>
           </div>
         </Form>
       </div>
--- app/services/s3client.server.ts
+++ app/services/s3client.server.ts
@@ -1,4 +1,4 @@
-import { S3Client } from "@aws-sdk/client-s3";
+import { HeadObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { Upload } from "@aws-sdk/lib-storage";
 import {
   unstable_composeUploadHandlers,
@@ -7,15 +7,16 @@ import {
 } from "@remix-run/node";
 import { PassThrough } from "stream";
 
+const s3Client = new S3Client({
+  endpoint: process.env.AWS_ENDPOINT,
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
 const uploadStream = (Key: string) => {
-  const s3Client = new S3Client({
-    endpoint: process.env.AWS_ENDPOINT,
-    region: process.env.AWS_REGION,
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    },
-  });
   const pass = new PassThrough();
   const upload = new Upload({
     client: s3Client,
@@ -26,6 +27,7 @@ const uploadStream = (Key: string) => {
     },
     leavePartsOnError: false,
   });
+
   return {
     writeStream: pass,
     promise: upload.done(),
@@ -40,18 +42,22 @@ const uploadImageToS3 = async (
   await writeAsyncIterableToWritable(data, stream.writeStream);
   // TODO: Figure out why file insists Location isn't on the object
   const file = (await stream.promise) as any;
-  return file.Location;
+  const metadata = await s3Client.send(
+    new HeadObjectCommand({ Bucket: file.Bucket, Key: file.Key })
+  );
+  console.log({ file, metadata });
+  return { location: file.Location, size: metadata.ContentLength };
 };
 
 const s3UploadHandler = unstable_composeUploadHandlers(
   // our custom upload handler
-  async ({ name, contentType, data, filename }) => {
+  async ({ name, data, filename }) => {
     if (name !== "podcastFile") {
       return undefined;
     }
     const keyName = filename ?? `${name}${+new Date()}`;
-    const uploadedImage = await uploadImageToS3(data, keyName);
-    return uploadedImage;
+    const uploadedImage: any = await uploadImageToS3(data, keyName);
+    return JSON.stringify(uploadedImage);
   },
   // fallback to memory for everything else
   unstable_createMemoryUploadHandler()
