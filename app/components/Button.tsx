cdc921a9047b1d5843019bd5568d58fb634a8b08
Chris Parsons <chris@chris-parsons.com>
2022-08-05 23:02:17 -0700

    Setting default on button component.

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,10 +1,14 @@
 type ButtonProps = {
   children: string;
   type: "submit" | "reset" | "button";
-  disabled: boolean;
+  disabled?: boolean;
 };
 
-export default function Button({ children, type, disabled }: ButtonProps) {
+export default function Button({
+  children,
+  type,
+  disabled = false,
+}: ButtonProps) {
   return (
     <button
       className="focus-visible:ring-offset-2zd inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500"
