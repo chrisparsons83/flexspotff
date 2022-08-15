2eac13517ab89749c0014d2412c17b0ba6ff05ae
Chris Parsons <chris@chris-parsons.com>
2022-08-14 22:57:48 -0700

    Add status check on dashboard.

--- app/routes/games/f-squared/index.tsx
+++ app/routes/games/f-squared/index.tsx
@@ -1,6 +1,30 @@
+import type { LoaderArgs } from "@remix-run/node";
 import { Link } from "@remix-run/react";
+import { authenticator } from "~/auth.server";
+import { getEntryByUserAndYear } from "~/models/fsquared.server";
+import { CURRENT_YEAR } from "~/utils/constants";
+import { superjson, useSuperLoaderData } from "~/utils/data";
+
+type LoaderData = {
+  existingEntry: Awaited<ReturnType<typeof getEntryByUserAndYear>> | null;
+};
+
+export const loader = async ({ request }: LoaderArgs) => {
+  const user = await authenticator.isAuthenticated(request);
+
+  const existingEntry = user
+    ? await getEntryByUserAndYear(user.id, CURRENT_YEAR)
+    : null;
+
+  return superjson<LoaderData>(
+    { existingEntry },
+    { headers: { "x-superjson": "true" } }
+  );
+};
 
 export default function FSquaredIndex() {
+  const { existingEntry } = useSuperLoaderData<typeof loader>();
+
   return (
     <>
       <h2>F²</h2>
@@ -15,7 +39,7 @@ export default function FSquaredIndex() {
       </p>
       <div>
         <h3>My entry</h3>
-        <p>Status: Not Entered</p>
+        <p>Status: {existingEntry ? `Submitted` : `Not Submitted`}</p>
         <p>
           <Link to="my-entry">View/Edit My Entry</Link>
         </p>
