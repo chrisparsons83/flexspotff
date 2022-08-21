89faf5080ddbf5f8c482e2a0b6d9d5d88435493f
Chris Parsons <chris@chris-parsons.com>
2022-08-20 23:30:59 -0700

    Get the form bit dynamic

--- app/routes/games/spread-pool/$year/$week.tsx
+++ app/routes/games/spread-pool/$year/$week.tsx
@@ -1,17 +1,42 @@
+import { useState } from "react";
+
 export default function GamesSpreadPoolWeek() {
+  const initialBudget = 1200;
+  const teams = ["Denver (+4.5)", "Miami (-4.5)"];
+  const [betAmount, setBetAmount] = useState<{
+    teamName: string;
+    amount: number;
+  }>({ teamName: "", amount: 0 });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const actualValue = Number.parseInt(e.target.value);
+    const amount = Math.abs(actualValue);
+    const teamName =
+      actualValue > 0 ? teams[1] : actualValue < 0 ? teams[0] : "No Bet";
+    setBetAmount({
+      teamName,
+      amount,
+    });
+  };
+
+  const betDisplay =
+    betAmount.amount !== 0
+      ? `${betAmount.amount} on ${betAmount.teamName}`
+      : "No Bet";
+
   return (
     <>
       <h2>Week Entry</h2>
       <div className="mb-4">
-        <div>Available to bet: 1180</div>
-        <div>Amount currently bet: 20</div>
+        <div>Available to bet: {initialBudget - betAmount.amount}</div>
+        <div>Amount currently bet: {betAmount.amount}</div>
       </div>
       <div className="grid md:grid-cols-2">
         <div>
           <div className="flex gap-2 justify-between">
-            <div className="w-1/3">Denver (+4.5)</div>
+            <div className="w-1/3">{teams[0]}</div>
             <div className="text-center">vs.</div>
-            <div className="w-1/3 text-right">Miami (-4.5)</div>
+            <div className="w-1/3 text-right">{teams[1]}</div>
           </div>
           <input
             type="range"
@@ -21,8 +46,9 @@ export default function GamesSpreadPoolWeek() {
             name="denverVsMiami"
             defaultValue={0}
             className="w-full"
+            onChange={handleChange}
           />
-          <div>Current Bet: 20 on Miami (-4.5)</div>
+          <div>Current Bet: {betDisplay}</div>
         </div>
       </div>
     </>
