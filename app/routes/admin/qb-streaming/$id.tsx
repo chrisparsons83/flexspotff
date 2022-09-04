52d77d78e07f8a2608f0f65b1920cf3f265373a7
Chris Parsons <chris@chris-parsons.com>
2022-09-03 23:05:49 -0700

    Setup action function for QB streaming admin. #32.

--- app/routes/admin/qb-streaming/$id.tsx
+++ app/routes/admin/qb-streaming/$id.tsx
@@ -1,18 +1,49 @@
-import type { LoaderArgs } from "@remix-run/node";
-import { Form, useTransition } from "@remix-run/react";
+import type { ActionArgs, LoaderArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Form, useActionData, useTransition } from "@remix-run/react";
 
 import type { Player } from "~/models/players.server";
 import { getActivePlayersByPosition } from "~/models/players.server";
 import type { QBStreamingWeek } from "~/models/qbstreamingweek.server";
 import { getQBStreamingWeek } from "~/models/qbstreamingweek.server";
 
+import Alert from "~/components/ui/Alert";
 import Button from "~/components/ui/Button";
 import { authenticator, requireAdmin } from "~/services/auth.server";
 import { superjson, useSuperLoaderData } from "~/utils/data";
 
+type ActionData = {
+  formError?: string;
+  message?: string;
+};
+
 type LoaderData = {
   activeQBs: Player[];
   qbStreamingWeek: QBStreamingWeek;
+};
+
+export const action = async ({ request }: ActionArgs) => {
+  const user = await authenticator.isAuthenticated(request, {
+    failureRedirect: "/login",
+  });
+  requireAdmin(user);
+
+  const formData = await request.formData();
+  const action = formData.get("_action");
+
+  switch (action) {
+    case "addPlayer": {
+      return json<ActionData>({ message: "Player has been added." });
+    }
+    case "removePlayer": {
+      return json<ActionData>({ message: "Player has been removed." });
+    }
+    case "updateWeek": {
+      return json<ActionData>({ message: "Week has been updated." });
+    }
+  }
+
+  return json<ActionData>({ message: "Nothing has happened." });
 };
 
 export const loader = async ({ params, request }: LoaderArgs) => {
@@ -36,12 +67,14 @@ export const loader = async ({ params, request }: LoaderArgs) => {
 };
 
 export default function AdminSpreadPoolYearWeek() {
+  const actionData = useActionData<ActionData>();
   const { activeQBs } = useSuperLoaderData<typeof loader>();
   const transition = useTransition();
 
   return (
     <div>
       <h2>Edit Picks for Week</h2>
+      {actionData?.message && <Alert message={actionData.message} />}
       <Form method="post">
         <h3>Add Player</h3>
         <div>
@@ -77,8 +110,10 @@ export default function AdminSpreadPoolYearWeek() {
             Add Player
           </Button>
         </div>
-        <h3>Available Players</h3>
-        <h3>Week Settings</h3>
+      </Form>
+      <h3>Available Players</h3>
+      <h3>Week Settings</h3>
+      <Form method="post">
         <label htmlFor="isOpen">
           <input type="checkbox" name="isOpen" id="isOpen" /> Week is active for
           selections
