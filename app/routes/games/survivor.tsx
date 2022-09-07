b33b99e2ff01bc7ac0be84741a2710c827ad0449
Chris Parsons <chris@chris-parsons.com>
2022-09-07 00:17:16 -0700

    Add survivor link to website.

--- app/routes/games/survivor.tsx
+++ app/routes/games/survivor.tsx
@@ -1,3 +1,9 @@
 export default function Streaming() {
-  return <div>Link to be pasted here</div>;
+  return (
+    <div>
+      <a href="https://football.fantasysports.yahoo.com/survival/register/joinprivategroup?gid=6184&invitation_key=26826166d578d3d4">
+        Join 2022 Survivor Group on Yahoo
+      </a>
+    </div>
+  );
 }
