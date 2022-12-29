864fe5e993056b18a0a0f0e50c43004534685990
Chris Parsons <chris@chris-parsons.com>
2022-12-29 12:52:35 -0800

    Fixing build again.

--- app/routes/podcast.tsx
+++ app/routes/podcast.tsx
@@ -19,21 +19,30 @@ export const loader = async ({ request }: LoaderArgs) => {
   );
 };
 
-
 export default function Records() {
   const { episodes } = useSuperLoaderData<typeof loader>();
   return (
     <div className="text-center md:text-left">
-
       <h2 id="Title">Trash Turtle Football</h2>
 
-      <img alt="podcast image" src="/ttflogowords.jpg" className="h-36 w-36 lg:w-56 lg:h-56 mx-auto md:ml-4 mb-4 float-none md:float-right"></img>
-      <p className="my-1 font-semibold" id='Hosts'>DrTrashdad and Bootzfantasy</p>
+      <img
+        alt="Trash Turtle Football logo"
+        src="/ttflogowords.jpg"
+        className="h-36 w-36 lg:w-56 lg:h-56 mx-auto md:ml-4 mb-4 float-none md:float-right"
+      ></img>
+      <p className="my-1 font-semibold" id="Hosts">
+        DrTrashdad and Bootzfantasy
+      </p>
 
-      <p id='Decription'>Unleashed from the iconic Flexspot Fantasy Football Discord server, Dr. Trashdad and Bootz bring the next generation of a fantasy football podcast.
-        A conversation between a numbers and spreadsheets analyst and a film room or gut drafter, Bootz and Trashdad talk about fantasy
-        relevant topics to help you get the edge you need in your league. Redraft, dynasty, bestball, and even some sportsbetting are common topics.
-        Listen to us to get in-depth and less talked-about fantasy knowledge without the fluff.
+      <p id="Decription">
+        Unleashed from the iconic Flexspot Fantasy Football Discord server, Dr.
+        Trashdad and Bootz bring the next generation of a fantasy football
+        podcast. A conversation between a numbers and spreadsheets analyst and a
+        film room or gut drafter, Bootz and Trashdad talk about fantasy relevant
+        topics to help you get the edge you need in your league. Redraft,
+        dynasty, bestball, and even some sportsbetting are common topics. Listen
+        to us to get in-depth and less talked-about fantasy knowledge without
+        the fluff.
       </p>
 
       {episodes.map((episode) => (
