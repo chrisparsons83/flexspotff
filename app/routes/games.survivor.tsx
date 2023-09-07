66bc3a1109239c0e801062354cd2f273df3a1ebd
Chris Parsons <438676+chrisparsons83@users.noreply.github.com>
2023-09-07 16:45:16 -0700

    Update survivor link for 2023.

--- app/routes/games.survivor.tsx
+++ app/routes/games.survivor.tsx
@@ -1,8 +1,8 @@
 export default function Streaming() {
   return (
     <div>
-      <a href="https://football.fantasysports.yahoo.com/survival/register/joinprivategroup?gid=6184&invitation_key=26826166d578d3d4">
-        Join 2022 Survivor Group on Yahoo
+      <a href="https://football.fantasysports.yahoo.com/survival/register/joinprivategroup?gid=24316&invitation_key=980689c7ecbe89b8">
+        Join 2023 Survivor Group on Yahoo
       </a>
     </div>
   );
