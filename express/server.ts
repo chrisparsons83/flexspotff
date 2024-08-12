6175fad5c67232f766ce24b481fb3eb77c7b5f99
Chris Parsons <chrisparsons1127@gmail.com>
2024-08-11 23:41:53 -0700

    Remove env logging.

--- express/server.ts
+++ express/server.ts
@@ -7,8 +7,6 @@ import path from "path";
 
 const app = express();
 
-console.log({ env: process.env });
-
 app.use((req, res, next) => {
   // helpful headers:
   res.set("x-fly-region", process.env.FLY_REGION ?? "unknown");
