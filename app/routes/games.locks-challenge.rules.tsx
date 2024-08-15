77685a420e688208349adc5c27deb4ad2f487d1b
Arsh Patel <arshpatel2001@gmail.com>
2024-08-15 10:44:54 -0400

    Added clarity locks challenge rules

--- app/routes/games.locks-challenge.rules.tsx
+++ app/routes/games.locks-challenge.rules.tsx
@@ -2,12 +2,23 @@ export default function GamesLocksChallengeStreamingRules() {
     return (
       <>
         <h2>Locks Challenge Rules</h2>
+
         <p>
-          Start the season with 0 points. Each week, you can select as many teams to win
-          as you want to. Each correct pick will count as one point. If any of your selected
-          teams does not win that week, you will be awarded 0 points for the entire week. Tie games
-          will not count as a point, but will also not result in 0 points for the week. Missing a 
-          week will not result in any penalty. Highest total at the end of NFL regular season wins.
+        Each week, you get to pick the winner of as many games as you'd like to choose. 
+        At the end of the week, if none of the teams you selected lost, you get one point per team you selected that won.
+        </p>
+
+        <p>
+        However, if any of your teams lose in that week, you score zero points for that week.
+        </p>
+
+        <p>
+        Ties don't count as a loss, but you don't get any points for a win from them either.
+        </p>
+
+        <p>
+        The player that scores the most points over the course of the season wins. 
+        There are no penalties for not picking any teams in a week besides foregoing the opportunity to earn points that week.
         </p>
       </>
     );
