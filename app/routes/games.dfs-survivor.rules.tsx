d8a93892588c1987a5f0534eb98c1ae9317a1470
Arsh Patel <arshpatel2001@gmail.com>
2024-12-10 18:35:51 -0500

    Added rules

---
+++ app/routes/games.dfs-survivor.rules.tsx
@@ -0,0 +1,28 @@
+export default function GamesDfsSurvivorRules() {
+    return (
+      <>
+        <h2>DFS Survivor Rules</h2>
+  
+        <p>
+          Each week, you will select a lineup consisting of 2 QBs, 2 RBs, 2 WRs, 1 TE, 1 FLEX, 1 K, and 1 DST. 
+          At the end of the week, your lineup's total points will be calculated and added to your overall score.
+        </p>
+  
+        <p>
+          You have until the start of a player's game to add or remove them from your lineup.
+          You cannot reuse any players you have already selected in previous weeks.
+        </p>
+  
+        <p>
+          Scoring will follow the same system as the Discord leagues. 
+          For more details, please refer to the <a href="/leagues/rules">league's scoring rules</a>.
+        </p>
+  
+        <p>
+          The player with the most points at the end of week 17 will be declared the winner. 
+          There are no penalties for missing a week or leaving a player slot empty.
+        </p>
+      </>
+    );
+  }
+  
