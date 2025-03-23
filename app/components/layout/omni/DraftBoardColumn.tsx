7475504ecb38f0cbc9565402998a4f0f3938f811
Chris Parsons <chrisparsons1127@gmail.com>
2025-03-22 23:30:45 -0700

    Add color coding on draft board for omni.

--- app/components/layout/omni/DraftBoardColumn.tsx
+++ app/components/layout/omni/DraftBoardColumn.tsx
@@ -1,4 +1,21 @@
+import clsx from 'clsx';
 import type { DraftBoardColumnProps } from '~/models/omniseason.server';
+
+const getBackgroundColor = (points: number, isComplete: boolean) => {
+  if (points === 80) {
+    return 'bg-green-600 bg-opacity-80';
+  } else if (points >= 50) {
+    return 'bg-green-600 bg-opacity-50';
+  } else if (points >= 30) {
+    return 'bg-green-600 bg-opacity-30';
+  } else if (points > 0) {
+    return 'bg-green-600 bg-opacity-20';
+  } else if (isComplete) {
+    return 'bg-red-600 bg-opacity-30';
+  } else {
+    return 'bg-slate-600';
+  }
+};
 
 const DraftBoardColumn = ({ omniTeam }: DraftBoardColumnProps) => {
   const avatarImage =
@@ -18,22 +35,35 @@ const DraftBoardColumn = ({ omniTeam }: DraftBoardColumnProps) => {
           {omniTeam.user?.discordName}
         </div>
       </div>
-      {omniTeam.draftPicks.map(draftPick => (
-        <div key={draftPick.id} className='h-16 p-1 bg-slate-600 flex'>
-          <div className='flex flex-col justify-between flex-4'>
-            <div className='font-bold line-clamp-2'>
-              {draftPick.player?.displayName}
+      {omniTeam.draftPicks.map(draftPick => {
+        return (
+          <div
+            key={draftPick.id}
+            className={clsx(
+              getBackgroundColor(
+                draftPick.player?.pointsScored || 0,
+                draftPick.player?.isComplete || false,
+              ),
+              'h-16 p-1 bg-slate-600 flex',
+            )}
+          >
+            <div className='flex flex-col justify-between flex-4'>
+              <div className='font-bold line-clamp-2'>
+                {draftPick.player?.displayName}
+              </div>
+              <div className='line-clamp-1'>
+                {draftPick.player?.sport.shortName}
+              </div>
             </div>
-            <div className='line-clamp-1'>
-              {draftPick.player?.sport.shortName}
+            <div className='flex-1 text-right flex flex-col justify-between'>
+              <div className='text-xs text-slate-500'>
+                {draftPick.pickNumber}
+              </div>
+              <div className='text-2xl'>{draftPick.player?.sport.emoji}</div>
             </div>
           </div>
-          <div className='flex-1 text-right flex flex-col justify-between'>
-            <div className='text-xs text-slate-500'>{draftPick.pickNumber}</div>
-            <div className='text-2xl'>{draftPick.player?.sport.emoji}</div>
-          </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
