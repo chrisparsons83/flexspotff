7276304152ad418eed670e494905d0f757c39464
Chris Parsons <chrisparsons1127@gmail.com>
2025-04-13 21:40:51 -0700

    Rules clarification on qualifying points.

--- app/routes/omni-league.2025.rules.tsx
+++ app/routes/omni-league.2025.rules.tsx
@@ -81,7 +81,8 @@ const OmniYearRules = () => {
         divided equally). If 8th, 9th, and 10th are tied, each of those players
         will earn 7 points (20+0+0 divided equally, rounded to the nearest
         point). This applies to both points awarded and golf and tennis
-        qualifying points.
+        qualifying points, though those are rounded to the nearest hundredth of
+        a point as opposed to the nearest point.
       </p>
       <p>
         3.4. The top eight in this season's sports are defined in the following
